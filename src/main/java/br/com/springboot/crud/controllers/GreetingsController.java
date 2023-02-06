dbed2e97ddce72f2cf45ff3337d506d654145c98
Alaniel <alanielnascimento@gmail.com>
2023-02-06 14:55:36 -0300

    Criando o Repository com Spring Data

--- src/main/java/br/com/springboot/crud/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/crud/controllers/GreetingsController.java
@@ -1,5 +1,6 @@
 package br.com.springboot.crud.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +8,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.crud.model.Usuario;
+import br.com.springboot.crud.repository.UsuarioRepository;
+
 /**
  *
  * A sample greetings controller to return greeting text
  */
 @RestController
 public class GreetingsController {
+	
+	@Autowired /*IC/CD ou CDI - Injeção de dependencia*/
+	private UsuarioRepository usuarioRepository;
+	
     /**
      *
      * @param name the name to greet
@@ -21,6 +29,10 @@ public class GreetingsController {
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(name);
+    	usuarioRepository.save(usuario);
         return "Hello " + name + "!";
     }
 }
--- src/main/java/br/com/springboot/crud/model/User.java
+++
@@ -1,5 +0,0 @@
-package br.com.springboot.crud.model;
-
-public class User {
-
-}
