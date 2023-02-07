29d8adb87c75700eed4c281ca693cf159c53ca7d
Alaniel <alanielnascimento@gmail.com>
2023-02-07 14:39:43 -0300

    Criando END-POINT de listar todos usuários

--- src/main/java/br/com/springboot/crud/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/crud/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.springboot.crud.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,4 +40,15 @@ public class GreetingsController {
     	usuarioRepository.save(usuario);
         return "Hello " + name + "!";
     }
+    
+    @GetMapping(value = "listartodos")
+    @ResponseBody /*RETORNAR OS DADOS PARA O CORPO DA RESPOSTA*/
+    public ResponseEntity<List<Usuario>> listarUsuario() {
+    	
+    	List<Usuario> usuarios = usuarioRepository.findAll(); /*EXECULTA A CONSULTA NO BANCO DE DADOS*/
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);/*RETORNAR A LISTA EM JSON*/
+    }
+    
+    
 }
